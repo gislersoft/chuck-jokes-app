f23fdb791421de80fa7c2c6c8f12849bcced6661
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-14 08:26:54 -0500

    Setting up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { FavoritesPageComponent } from './favorites/favorites-page/favorites-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'favorites', component: FavoritesPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
