8481de708798ede5d5b23d9963ef5029f0c876f3
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-14 10:53:56 -0500

    Fixing unit test and preparing commands for github actions

--- src/app/services/chuck-api.service.spec.ts
+++ src/app/services/chuck-api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ChuckAPIService } from './chuck-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ChuckAPIService', () => {
   let service: ChuckAPIService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(ChuckAPIService);
   });
 
