a18a912fbad0892a6601bf7deccb4181589c0055
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-15 11:14:56 -0500

    Starting refactor of home to have a joke card

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JokeCardComponent } from './joke-card/joke-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    JokeCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommonModule { }
