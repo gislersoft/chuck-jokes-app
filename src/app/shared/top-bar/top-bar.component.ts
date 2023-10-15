059821231a7690b8fcf15211649ac61eb22997b3
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-15 12:43:57 -0500

    Adding zero state and multiple improvements

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
+  @Input()
+  public currentPage: string = '';
 
   constructor(private router: Router) {}
 
