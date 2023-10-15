3a3efb5b87c5be79e5373698e9097af3f4643bdd
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-15 17:33:25 -0500

    Adding unit test to the jokes storage service

--- src/app/services/jokes-storage.service.spec.ts
+++ src/app/services/jokes-storage.service.spec.ts
@@ -1,8 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
 import { JokesStorageService } from './jokes-storage.service';
+import { chuckJokesMock } from '../../tests/local-storage-data.mock';
+import { Joke } from './types/joke.type';
 
-describe('JokesStorageService', () => {
+const jokeMock: Joke = {
+  categories: [],
+  created_at: '2020-01-05 13:42:25.099703',
+  icon_url: 'https://assets.chucknorris.host/img/avatar/chuck-norris.png',
+  id: 'fhajskdfhaksfha7788',
+  updated_at: '2020-01-05 13:42:25.099703',
+  url: 'https://api.chucknorris.io/jokes/fhajskdfhaksfha7788',
+  value:
+    'Chuck Norris develop without unit test.'
+};
+
+fdescribe('JokesStorageService', () => {
   let service: JokesStorageService;
 
   beforeEach(() => {
@@ -13,4 +25,69 @@ describe('JokesStorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('when there are jokes in the localstorage', () => {
+    beforeEach(() =>{
+      localStorage.clear();
+      localStorage.setItem(JokesStorageService.STORE_JOKE_KEY, JSON.stringify(chuckJokesMock));
+    });
+    it('should load jokes from the localstorage', () => {
+      const currentJokes = service.getJokes();
+      expect(currentJokes).toEqual(chuckJokesMock);
+    });
+    it('should add a joke to the list', () => {
+      service.addJoke(jokeMock);
+      const currentJokes = service.getJokes();
+      const jokesCompare = [...chuckJokesMock];
+      jokesCompare.push(jokeMock);
+      expect(currentJokes).toEqual(jokesCompare);
+    });
+    it('should remove a joke from the list', () => {
+      service.addJoke(jokeMock);
+      service.removeJoke(jokeMock);
+      const currentJokes = service.getJokes();
+      expect(currentJokes).toEqual(chuckJokesMock);
+    });
+    afterAll(()=>{
+      localStorage.clear();
+    });
+  });
+
+  describe('when multiple jobs are added or removed', () => {
+    beforeAll(() =>{
+      localStorage.clear();
+      localStorage.setItem(JokesStorageService.STORE_JOKE_KEY, JSON.stringify(chuckJokesMock));
+    });
+    it('should not add the same joke', () => {
+      service.addJoke(jokeMock);
+      service.addJoke(jokeMock);
+      service.addJoke(jokeMock);
+      const currentJokes = service.getJokes();
+      const jokesCompare = [...chuckJokesMock];
+      jokesCompare.push(jokeMock);
+      expect(currentJokes).toEqual(jokesCompare);
+    });
+    it('should remove the joke', () => {
+      service.addJoke(jokeMock);
+      service.removeJoke(jokeMock);
+      service.removeJoke(jokeMock);
+      service.removeJoke(jokeMock);
+      const currentJokes = service.getJokes();
+      expect(currentJokes).toEqual(chuckJokesMock);
+    });
+    afterAll(()=>{
+      localStorage.clear();
+    });
+  });
+
+  describe('when localstorage is empty', () => {
+    beforeEach(() =>{
+      localStorage.clear();
+    });
+    it('should load an empty array of jokes', () => {
+      const currentJokes = service.getJokes();
+      expect(currentJokes).toEqual([]);
+    });
+  });
+
 });
---
+++ src/tests/local-storage-data.mock.ts
@@ -0,0 +1,36 @@
+import { Joke } from "src/app/services/types/joke.type";
+
+export const chuckJokesMock: Joke[] = [
+  {
+    categories: [],
+    created_at: '2020-01-05 13:42:30.730109',
+    icon_url: 'https://assets.chucknorris.host/img/avatar/chuck-norris.png',
+    id: '1a0W_WfuSrGyob5pG2lSuw',
+    updated_at: '2020-01-05 13:42:30.730109',
+    url: 'https://api.chucknorris.io/jokes/1a0W_WfuSrGyob5pG2lSuw',
+    value: 'Chuck Norris just look at him..',
+    timestamp: 1697406449995,
+  },
+  {
+    categories: [],
+    created_at: '2020-01-05 13:42:28.664997',
+    icon_url: 'https://assets.chucknorris.host/img/avatar/chuck-norris.png',
+    id: 'XXgyPnF9RjCiDsMyAPIF-w',
+    updated_at: '2020-01-05 13:42:28.664997',
+    url: 'https://api.chucknorris.io/jokes/XXgyPnF9RjCiDsMyAPIF-w',
+    value:
+      "Chuck Norris' Chihuahua, Pickles, had to be placed in quarantine after she chewed up Rocco, the neighbors Pit bull.",
+    timestamp: 1697406445037,
+  },
+  {
+    categories: [],
+    created_at: '2020-01-05 13:42:25.099703',
+    icon_url: 'https://assets.chucknorris.host/img/avatar/chuck-norris.png',
+    id: 'pU4HxBSgRwCaRtld9fS5dg',
+    updated_at: '2020-01-05 13:42:25.099703',
+    url: 'https://api.chucknorris.io/jokes/pU4HxBSgRwCaRtld9fS5dg',
+    value:
+      'Chuck Norris was once drafted to the Boston Red Sox, but was released because he insisted on using the umpire as a bat.',
+    timestamp: 1697406439992,
+  },
+];
