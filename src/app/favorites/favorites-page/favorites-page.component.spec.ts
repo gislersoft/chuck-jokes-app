ce07387d8c3c9cae712bda5465a4b0a0b794210e
Gisler Garces <gisler.garces@sourcemeridian.com>
2023-10-15 19:46:15 -0500

    Adding more unit test to favorites page

--- src/app/favorites/favorites-page/favorites-page.component.spec.ts
+++ src/app/favorites/favorites-page/favorites-page.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesPageComponent } from './favorites-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { JokesStorageService } from 'src/app/services/jokes-storage.service';
+import { chuckJokesMock } from 'src/tests/local-storage-data.mock';
 
 describe('FavoritesPageComponent', () => {
   let component: FavoritesPageComponent;
@@ -19,9 +21,20 @@ describe('FavoritesPageComponent', () => {
     fixture = TestBed.createComponent(FavoritesPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    localStorage.clear();
+    localStorage.setItem(JokesStorageService.STORE_JOKE_KEY, JSON.stringify(chuckJokesMock));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load the favorites jokes', () => {
+    expect(component.jokesList.length).toBe(10);
+  });
+
+  it('should delete a favorite joke', () => {
+    component.deleteJoke(chuckJokesMock[0]);
+    expect(component.jokesList.length).toBe(9);
   });
 });
